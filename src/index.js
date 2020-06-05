61fc1480c3022ca0acde2b88a101acd0794b9bc4
nrdurkin <nrdurkin3@gmail.com>
2020-06-05 09:07:53 -0400

    Added internal role functionality and mod tools + prettier changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import { directiveSetup } from './directives/index';
-import { serviceSetup } from './services/index';
-import { filterSetup } from './filter';
-import routing from './views/index';
-import './styles/prettify.css';
-import './styles/main.css';
-import './styles/mobile.css';
+import { directiveSetup } from "./directives/index";
+import { serviceSetup } from "./services/index";
+import { filterSetup } from "./filter";
+import routing from "./views/index";
+import "./styles/prettify.css";
+import "./styles/main.css";
+import "./styles/mobile.css";
 
-var mod = angular.module("kmapp", ['ngSanitize', 'ui.router'])
-    .config(routing);
+var mod = angular.module("kmapp", ["ngSanitize", "ui.router"]).config(routing);
 
 directiveSetup(mod);
 serviceSetup(mod);
 filterSetup(mod);
 
-mod.$inject = ['$http', '$rootScope'];
-mod.run(function($http, $rootScope) {
-    $http.defaults.headers.common["X-UserToken"] = g_ck;
-    $rootScope.isAdmin = g_user.hasRole('x_nuvo_ko.mod');
+mod.$inject = ["$http", "$rootScope"];
+mod.run(function ($http, $rootScope) {
+  $http.defaults.headers.common["X-UserToken"] = g_ck;
+  $rootScope.isAdmin = g_user.hasRole("x_nuvo_ko.mod");
+  $rootScope.isInternal = g_user.hasRole("x_nuvo_ko.internal");
 });
-
-
-
 
 export { mod };
--- src/views/edit.js
+++ src/views/edit.js
@@ -1,90 +1,128 @@
-import template from '../templates/viewEdit.html';
+import template from "../templates/viewEdit.html";
 export function edit_view_def() {
+  var edit = {
+    template: template,
+    controller: edit_ctrl,
+    controllerAs: "vm",
+    scope: true,
+    url: "/questions/{qid}/edit?aid"
+  };
+  edit_ctrl.$inject = [
+    "$stateParams",
+    "$scope",
+    "$element",
+    "$state",
+    "questionService",
+    "createService",
+    "tagsService"
+  ];
+  function edit_ctrl(
+    $stateParams,
+    $scope,
+    $element,
+    $state,
+    questionService,
+    createService,
+    tagsService
+  ) {
+    var vm = this;
+    vm.aid = $stateParams.aid;
+    vm.qid = $stateParams.qid;
+    vm.type = vm.aid === undefined ? "q" : "a";
+    vm.noText = false;
 
-    var edit = {
-        template: template,
-        controller: edit_ctrl,
-        controllerAs: "vm",
-        scope: true,
-        url: '/questions/{qid}/edit?aid'
+    //Load the question or answer data
+    questionService.get(vm.qid).then(function (data) {
+      if (!data) vm.noText = true;
+      else if (vm.type == "a") {
+        for (var i = 0; i < data.answers.length; i++) {
+          if (data.answers[i].answer_id == vm.aid)
+            vm.editorText = data.answers[i].answer;
+        }
+      } else if (vm.type == "q" && data.question) {
+        vm.questionTitle = data.question;
+        vm.tagList = data.tags;
+        vm.editorText = data.description;
+        vm.internal = data.internal;
+      }
+      if (!vm.editorText) vm.noText = true;
+    });
+
+    //Post new data to tables, reload question page
+    vm.update = function () {
+      function reload() {
+        $state.go("question", {
+          id: vm.qid
+        });
+      }
+      if (vm.editorText.length < 10)
+        alert(
+          (vm.type == "q" ? "Description" : "Answer") +
+            " length must be at least 10 characters"
+        );
+      else if (vm.editorText.length > 5000)
+        alert(
+          (vm.type == "q" ? "Description" : "Answer") +
+            " length must be less than 5000 characters. Current length is " +
+            vm.editorText.length +
+            " characters"
+        );
+      else {
+        if (vm.type == "q") {
+          let makeInternal = false;
+          if (vm.internal && vm.internal) makeInternal = true;
+          createService
+            .updateQuestion(
+              vm.qid,
+              vm.questionTitle,
+              vm.editorText,
+              vm.tagList,
+              makeInternal
+            )
+            .then(reload);
+        } else createService.updateAnswer(vm.aid, vm.editorText).then(reload);
+      }
     };
-    edit_ctrl.$inject = ['$stateParams', '$scope', '$element', '$state', 'questionService', 'createService', 'tagsService'];
-    function edit_ctrl($stateParams, $scope, $element, $state, questionService, createService, tagsService) {
-        var vm = this;
-        vm.aid = $stateParams.aid;
-        vm.qid = $stateParams.qid;
-        vm.type = vm.aid === undefined ? 'q' : 'a';
-        vm.noText = false;
 
-        //Load the question or answer data
-        questionService.get(vm.qid)
-            .then(function (data) {
-                if (!data) vm.noText = true;
-                else if (vm.type == 'a') {
-                    for (var i = 0; i < data.answers.length; i++) {
-                        if (data.answers[i].answer_id == vm.aid) vm.editorText = data.answers[i].answer;
-                    }
-                } else if (vm.type == 'q' && data.question) {
-                    vm.questionTitle = data.question;
-                    vm.tagList = data.tags;
-                    vm.editorText = data.description;
-                }
-                if (!vm.editorText) vm.noText = true;
-            });
+    //Again, logic for a tag input
+    vm.autofillList = [];
+    tagsService.autoSubscribe(function (tags) {
+      tags.forEach(function (tag) {
+        if (vm.autofillList.indexOf(tag.name) == -1)
+          vm.autofillList.push(tag.name);
+      });
+    });
 
-        //Post new data to tables, reload question page
-        vm.update = function () {
-            function reload() {
-                $state.go('question', {
-                    id: vm.qid
-                });
-            }
-            if (vm.editorText.length < 10) alert((vm.type == 'q' ? 'Description' : 'Answer') + " length must be at least 10 characters");
-            else if (vm.editorText.length > 5000) alert((vm.type == 'q' ? 'Description' : 'Answer') + " length must be less than 5000 characters. Current length is " + vm.editorText.length + " characters");
-            else {
-                if (vm.type == 'q') createService.updateQuestion(vm.qid, vm.questionTitle, vm.editorText, vm.tagList).then(reload);
-                else createService.updateAnswer(vm.aid, vm.editorText).then(reload);
-            }
-        };
+    $scope.$watch("vm.addedTag", function (newVal) {
+      if (newVal != "") tagsService.autofillTags(newVal);
+    });
 
-        //Again, logic for a tag input
+    $element[0].querySelector(".tags-input").on("blur", function () {
+      vm.addTag();
+    });
+
+    vm.addTag = function () {
+      if (vm.addedTag.length > 0) {
+        vm.tagList.push(vm.addedTag);
+        vm.tagList = vm.tagList.filter(function (item, pos) {
+          return vm.tagList.indexOf(item) == pos;
+        });
+        vm.addedTag = "";
         vm.autofillList = [];
-        tagsService.autoSubscribe(function (tags) {
-            tags.forEach(function (tag) {
-                if (vm.autofillList.indexOf(tag.name) == -1) vm.autofillList.push(tag.name);
-            });
-        });
+      }
+    };
 
-        $scope.$watch('vm.addedTag', function (newVal) {
-            if (newVal != '') tagsService.autofillTags(newVal);
-        });
+    vm.testAddTag = function (e) {
+      if (e.keyCode == 13) {
+        e.preventDefault();
+        vm.addTag();
+      }
+    };
 
-        $element[0].querySelector('.tags-input').on('blur', function () {
-            vm.addTag();
-        });
+    vm.removeTag = function (tag) {
+      vm.tagList.splice(vm.tagList.indexOf(tag), 1);
+    };
+  }
 
-        vm.addTag = function () {
-            if (vm.addedTag.length > 0) {
-                vm.tagList.push(vm.addedTag);
-                vm.tagList = vm.tagList.filter(function (item, pos) {
-                    return vm.tagList.indexOf(item) == pos;
-                });
-                vm.addedTag = "";
-                vm.autofillList = [];
-            }
-        };
-
-        vm.testAddTag = function (e) {
-            if (e.keyCode == 13) {
-                e.preventDefault();
-                vm.addTag();
-            }
-        };
-
-        vm.removeTag = function (tag) {
-            vm.tagList.splice(vm.tagList.indexOf(tag), 1);
-        };
-    }
-
-    return edit;
+  return edit;
 }
